30f6676a0a72c5391041d585cace041780f589fb
MiltMont <miltgonmont@pm.me>
2024-07-14 00:37:09 -0600

    fix: Modify access control for Categories collection

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,9 +1,16 @@
 import { CollectionConfig } from "payload/types";
+import { isAdmin } from "../access/isAdmin";
 
 export const Categories: CollectionConfig = {
   slug: "categories",
   admin: {
     useAsTitle: "name",
+  },
+  access: {
+    create: isAdmin,
+    read: () => true,
+    update: isAdmin,
+    delete: isAdmin,
   },
   fields: [
     {
